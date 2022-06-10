296aa4b3e03a9f72e0bb1eb7a4bf99d1bf959139
Sergey <zaycev.sergey@mail.ru>
2022-06-10 09:42:12 +0300

    Исправил внешний вид

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 //Функция получения рандомного целого числа из диапазона
-const getRandomNumber = (min, max) => (max < min || min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) ? 'Нижняя граница должна быть меньше верхней границы, границы должны быть целыми числами и не должны быть отрицательными числами' : (Math.floor(Math.random() * (max - min + 1)) + min);
+function getRandomNumber(min, max) {
+  if (max <= min || min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+    return 'Нижняя граница должна быть меньше верхней границы, границы должны быть целыми числами и не должны быть отрицательными числами';
+  }
+  return (Math.floor(Math.random() * (max - min + 1)) + min);
+
+}
 
 //Функция получения рандомного числа из диапазона, с указанием числа знаков после запятой
-const getRandomFractionalNumber = (min, max, count) => (max < min || min < 0 || max < 0 || count < 0 || !Number.isInteger(count)) ? 'Нижняя граница должна быть меньше верхней, границы не должны быть отрицательными числами, количество знаков после запятой должно быть не отрицательным целым числом' : Number(((Math.random() * (max - min)) + min).toFixed(count));
+function getRandomFractionalNumber(min, max, count) {
+  if (max <= min || min < 0 || max < 0 || count < 0 || !Number.isInteger(count)) {
+    return 'Нижняя граница должна быть меньше верхней, границы не должны быть отрицательными числами, количество знаков после запятой должно быть не отрицательным целым числом';
+  }
+  return (Number(((Math.random() * (max - min)) + min).toFixed(count)));
 
-getRandomNumber(23, 26);
+}
+getRandomNumber(26, 26);
 getRandomFractionalNumber(1, 8, 4);
