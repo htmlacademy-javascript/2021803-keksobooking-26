29e959e765e232b1b720ac787420aacf7cf73322
Sergey <zaycev.sergey@mail.ru>
2022-06-28 20:02:56 +0300

    Оставил только модуль создания объявлений

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
-import { createAdvertisement, COUNT_ADVERTISEMENTS } from './data.js';
-
-const ADVERTISEMENTS = [];
-for (let i = 1; i <= COUNT_ADVERTISEMENTS; i++) {
-  ADVERTISEMENTS.push(createAdvertisement(i));
-}
+import './create_advertisements.js';
 
