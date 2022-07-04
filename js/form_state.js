501fe319220e2e25b021bf0f464f237c571f7751
Sergey <zaycev.sergey@mail.ru>
2022-07-04 12:54:40 +0300

    Убрал сравнение с числами.

--- js/form_state.js
+++ js/form_state.js
@@ -1,15 +1,16 @@
 // Функция изменения состояния страницы:
 // 0 - неактивное состояние
 // 1 - активное состояние
+const FormState = { Active: 1, Inactive: 0 };
 const changeState = (state) => {
   const formAdvertisement = document.querySelector('.ad-form');
   const formFilterAdvertisement = document.querySelector('.map__filters');
-  if(state === 0) {
+  if(state === FormState.Inactive) {
     formAdvertisement.classList.add('ad-form--disabled');
     formFilterAdvertisement.classList.add('map__filters--disabled');
     formAdvertisement.setAttribute('disabled','disabled');
     formFilterAdvertisement.setAttribute('disabled','disabled');
-  } else if (state ===1) {
+  } else if (state === FormState.Active) {
     formAdvertisement.classList.remove('ad-form--disabled');
     formFilterAdvertisement.classList.remove('map__filters--disabled');
     formAdvertisement.removeAttribute('disabled');
