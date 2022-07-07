b9d4e52f876f3735647d37a42c7444491c289e4e
Sergey <zaycev.sergey@mail.ru>
2022-07-07 21:23:30 +0300

    импортировал не весь модуль, а только функцию

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {getRenderingAdvertisement} from './create_advertisements.js';
 import {createAdvertisements} from './data.js';
 import {changeState} from './form_state.js';
-import './form_validate.js';
+import {validateForm} from './form_validate.js';
 getRenderingAdvertisement(createAdvertisements());
 changeState(1);
+validateForm();
