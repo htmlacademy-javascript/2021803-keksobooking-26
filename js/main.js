3d018bc2ef4c97e7832b4d8b0746d09d5a1cc0b2
Sergey <zaycev.sergey@mail.ru>
2022-07-14 21:41:19 +0300

    Отрисовка объявлений с сервера

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 import {changeState} from './form_state.js';
 changeState(0);
-import {validateForm} from './form_validate.js';
+import {validateForm,setAdsFormSubmit} from './form_validate.js';
 import {getMap} from './map.js';
-getMap();
+import {getData} from './api.js';
 validateForm();
+getData((advertisements) => {
+  getMap(advertisements);
+});
+setAdsFormSubmit();
