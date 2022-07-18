5ff25c18d34f5c2fc37d755decece89802655b17
Keks <keks@htmlacademy.ru>
2022-07-18 23:30:24 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/data.js
@@ -0,0 +1,16 @@
+const TypesHouse = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
+
+const PriceLimit = {
+  any: [0, 100000],
+  low: [0, 10000],
+  middle: [10000, 50000],
+  high: [50000, 100000]
+};
+
+export {TypesHouse,PriceLimit};
